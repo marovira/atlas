ac343f5c4035648fb0dd5050b7f313cb9e4d0b7a
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2015-12-08 20:59:20 -0800

    [brief] Adds the Scene class to be used in conjuction with the Application class.
    [detailed]

---
+++ include/atlas/Scene.hpp
@@ -0,0 +1,31 @@
+#ifndef ATLAS_INCLUDE_ATLAS_SCENE_HPP
+#define ATLAS_INCLUDE_ATLAS_SCENE_HPP
+
+#pragma once
+
+#include "Atlas.hpp"
+#include "Matrix.hpp"
+
+namespace atlas
+{
+    class Scene
+    {
+    public:
+        Scene();
+        ~Scene();
+
+        virtual void mousePressEvent(int button, int action, int modifiers);
+        virtual void mouseMoveEvent(double xPos, double yPos);
+        virtual void keyPressEvent(int key, int scancode, int action,
+            int mods);
+        virtual void screenResizeEvent(int width, int height);
+        virtual void updateScene(double time);
+        virtual void renderScene();
+
+    protected:
+        Matrix4 mProjection;
+        Matrix4 mView;
+    };
+}
+
+#endif
---
+++ source/atlas/Scene.cpp
@@ -0,0 +1,56 @@
+#include "atlas/Scene.hpp"
+#include "atlas/Math.hpp"
+#include "atlas/Macros.hpp"
+#include "atlas/GLHeader.hpp"
+
+namespace atlas
+{
+    Scene::Scene()
+    { }
+
+    Scene::~Scene()
+    { }
+
+    void Scene::mousePressEvent(int button, int action, int modifiers)
+    {
+        UNUSED(button);
+        UNUSED(action);
+        UNUSED(modifiers);
+    }
+
+    void Scene::mouseMoveEvent(double xPos, double yPos)
+    {
+        UNUSED(xPos);
+        UNUSED(yPos);
+    }
+
+    void Scene::keyPressEvent(int key, int scancode, int action, int mods)
+    {
+        UNUSED(key);
+        UNUSED(scancode);
+        UNUSED(action);
+        UNUSED(mods);
+    }
+
+    void Scene::screenResizeEvent(int width, int height)
+    {
+        USING_GLM_NS;
+
+        glViewport(0, 0, width, height);
+        mProjection = perspective(radians(45.0), 
+            (double)width / height, 1.0, 1000.0);
+    }
+
+    void Scene::updateScene(double time)
+    {
+        UNUSED(time);
+    }
+
+    void Scene::renderScene()
+    {
+        float grey = 161.0f / 255.0f;
+        glClearColor(grey, grey, grey, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+    }
+}
