43943849c388ab4012d814b4c00316dab0f3f5d8
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2015-12-14 12:39:19 -0800

    [brief] Adds GLFW and Quaternion headers.
    [detailed]
    - Since Atlas doesn't give direct access to the GLFW headers, an
      intermediate header is added to encapsulate GLFW.
    - Adds quaternion GLM headers.

---
+++ include/atlas/GLFW.hpp
@@ -0,0 +1,8 @@
+#ifndef ATLAS_INCLUDE_ATLAS_GLFW_HPP
+#define ATLAS_INCLUDE_ATLAS_GLFW_HPP
+
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+#endif
---
+++ include/atlas/Quaternion.hpp
@@ -0,0 +1,13 @@
+#ifndef ATLAS_INCLUDE_ATLAS_QUATERNION_HPP
+#define ATLAS_INCLUDE_ATLAS_QUATERNION_HPP
+
+#pragma once
+
+#include <glm/gtc/quaternion.hpp>
+
+namespace atlas
+{
+    typedef glm::quat Quaternion;
+}
+
+#endif
