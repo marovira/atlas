94c3b02fe5aaa9e2390b8650536dffc24f93bbb6
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2015-12-14 14:50:30 -0800

    [brief] Adds the Time struct.
    [detailed]
    - This will be used for animating objects that require all the
      information regarding time from the application.

---
+++ include/atlas/Time.hpp
@@ -0,0 +1,16 @@
+#ifndef ATLAS_INCLUDE_ATLAS_TIME_HPP
+#define ATLAS_INCLUDE_ATLAS_TIME_HPP
+
+#pragma once
+
+namespace atlas
+{
+    struct Time
+    {
+        float totalTime;
+        float deltaTime;
+        float currentTime;
+    };
+}
+
+#endif
