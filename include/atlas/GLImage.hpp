12694e9db9d5ec26d208e8d085a4e8424e1e335d
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2015-12-17 21:05:42 -0800

    [brief] Adds header file for GLI headers used to load/save textures.
    [detailed]

---
+++ include/atlas/GLImage.hpp
@@ -0,0 +1,11 @@
+#ifndef ATLAS_INCLUDE_ATLAS_GL_IMAGE_HPP
+#define ATLAS_INCLUDE_ATLAS_GL_IMAGE_HPP
+
+#pragma once
+
+#include <gli/texture.hpp>
+#include <gli/load.hpp>
+#include <gli/save.hpp>
+#include <gli/gl.hpp>
+
+#endif
