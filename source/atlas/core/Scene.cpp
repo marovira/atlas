f24c88fa12ba7e98dab477d0346e1046e60e8343
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-01-05 17:27:15 -0800

    [brief] Updates include files.
    [detailed]

--- source/atlas/core/Scene.cpp
+++ source/atlas/core/Scene.cpp
@@ -1,61 +1,64 @@
-#include "atlas/Scene.hpp"
-#include "atlas/Math.hpp"
-#include "atlas/Macros.hpp"
-#include "atlas/GLHeader.hpp"
+#include "atlas/core/Scene.hpp"
+#include "atlas/math/Math.hpp"
+#include "atlas/core/Macros.hpp"
+#include "atlas/gl/GL.hpp"
 
 namespace atlas
 {
-    Scene::Scene()
-    { }
-
-    Scene::~Scene()
-    { }
-
-    void Scene::mousePressEvent(int button, int action, int modifiers)
+    namespace core
     {
-        UNUSED(button);
-        UNUSED(action);
-        UNUSED(modifiers);
-    }
+        Scene::Scene()
+        { }
 
-    void Scene::mouseMoveEvent(double xPos, double yPos)
-    {
-        UNUSED(xPos);
-        UNUSED(yPos);
-    }
+        Scene::~Scene()
+        { }
 
-    void Scene::keyPressEvent(int key, int scancode, int action, int mods)
-    {
-        UNUSED(key);
-        UNUSED(scancode);
-        UNUSED(action);
-        UNUSED(mods);
-    }
+        void Scene::mousePressEvent(int button, int action, int modifiers)
+        {
+            UNUSED(button);
+            UNUSED(action);
+            UNUSED(modifiers);
+        }
 
-    void Scene::screenResizeEvent(int width, int height)
-    {
-        USING_GLM_NS;
+        void Scene::mouseMoveEvent(double xPos, double yPos)
+        {
+            UNUSED(xPos);
+            UNUSED(yPos);
+        }
 
-        glViewport(0, 0, width, height);
-        mProjection = perspective(radians(45.0), 
-            (double)width / height, 1.0, 1000.0);
-    }
+        void Scene::keyPressEvent(int key, int scancode, int action, int mods)
+        {
+            UNUSED(key);
+            UNUSED(scancode);
+            UNUSED(action);
+            UNUSED(mods);
+        }
 
-    void Scene::updateScene(double time)
-    {
-        UNUSED(time);
-    }
+        void Scene::screenResizeEvent(int width, int height)
+        {
+            USING_GLM_NS;
 
-    void Scene::renderScene()
-    {
-        float grey = 161.0f / 255.0f;
-        glClearColor(grey, grey, grey, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glEnable(GL_DEPTH_TEST);
-    }
+            glViewport(0, 0, width, height);
+            mProjection = perspective(radians(45.0),
+                (double)width / height, 1.0, 1000.0);
+        }
 
-    bool Scene::sceneEnded()
-    {
-        return true;
+        void Scene::updateScene(double time)
+        {
+            UNUSED(time);
+        }
+
+        void Scene::renderScene()
+        {
+            float grey = 161.0f / 255.0f;
+            glClearColor(grey, grey, grey, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glEnable(GL_DEPTH_TEST);
+        }
+
+        bool Scene::sceneEnded()
+        {
+            return true;
+        }
     }
 }
