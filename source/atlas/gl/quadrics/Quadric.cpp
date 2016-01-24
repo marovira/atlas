f66db98e8247a14132c98faccb0b280fe706c148
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-01-23 21:52:39 -0800

    [brief] Introduces quadrics submodule of gl.
    [detailed]
    - The purpose of this submodule is to provide the same functionality
      present in the glu quadrics functions from OpenGL 2.

---
+++ source/atlas/gl/quadrics/Quadric.cpp
@@ -0,0 +1,22 @@
+#include "atlas/gl/quadrics/Quadric.hpp"
+
+namespace atlas
+{
+    namespace gl
+    {
+        namespace quadrics
+        {
+            Quadric::Quadric()
+            { }
+
+            Quadric::~Quadric()
+            { }
+
+            void Quadric::bindBuffers()
+            { }
+
+            void Quadric::unBindBuffers()
+            { }
+        }
+    }
+}
