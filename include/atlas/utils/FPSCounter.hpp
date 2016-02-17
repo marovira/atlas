15c3da9dabf88a4cce23b94802317a922bd0310c
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-02-16 21:35:50 -0800

    [brief] Adds documentation for FPSCounter
    [detailed]

--- include/atlas/utils/FPSCounter.hpp
+++ include/atlas/utils/FPSCounter.hpp
@@ -12,16 +12,56 @@ namespace atlas
 {
     namespace utils
     {
+        /**
+         * \class FPSCounter
+         * \brief Provides a way of setting the fps for a scene.
+         *  
+         * This class provides an easy way of handling timing events for both
+         * animations as well as rendering. It can be used to control when
+         * geometries are updated/rendered by simply adding an if statement
+         * inside the appropriate function in the scene class.
+         */
         class FPSCounter
         {
         public:
+            /**
+             * Standard empty constructor.
+             */
             FPSCounter();
+
+            /**
+             * Initializes the counter to the desired FPS.
+             *
+             * \param[in] fps The frames per second the counter will keep
+             * track of.
+             */
             FPSCounter(float fps);
+
+            /**
+             * Standard copy constructor.
+             */
             FPSCounter(FPSCounter const& t);
+
+            /**
+             * Standard destructor.
+             */
             ~FPSCounter();
 
+            /**
+             * Sets the FPS for the counter.
+             *
+             * \param[in] fps The frames per second the counter will keep
+             * track of.
+             */
             void setFPS(float fps);
 
+            /**
+             * Checks whether the required time has elapsed between 
+             * frames.
+             * 
+             * \return True if the next frame should be executed, 
+             * false otherwise.
+             */
             bool isFPS(Time& t) const;
 
         private:
