bb727777a833daaf87c0d49dedcb50f175d16a68
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-06-10 21:04:38 -0700

    [brief] Introduces STBImage to Atlas.
    [detailed]
    - This will replace GLI as the image loading library that will be used for
      textures.

---
+++ include/atlas/core/STBImage.hpp
@@ -0,0 +1,22 @@
+/**
+ * \file STBImage.hpp
+ * \brief A header wrapper for the stb_image header.
+ * \note Use
+ * \code
+ * #include <atlas/core/STBImage.hpp>
+ * \endcode
+ * instead of using
+ * \code
+ * #include <stb/stb_image.h>
+ * \endcode
+ * to prevent errors when/if the header changes.
+ */
+
+#ifndef ATLAS_INCLUDE_ATLAS_CORE_STB_IMAGE_HPP
+#define ATLAS_INCLUDE_ATLAS_CORE_STB_IMAGE_HPP
+
+#pragma once
+
+#include <stb/stb_image.h>
+
+#endif
---
+++ source/atlas/core/STBImage.cpp
@@ -0,0 +1,2 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "atlas/core/STBImage.hpp"
