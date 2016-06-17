e0fea4a391539308f8aad29cff91bb7b64c58fbd
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-06-16 21:52:58 -0700

    [brief] Adds OpenGL error callback.
    [detailed]
    - Removes the usage of a static function as a namespace and simply moves the
      contained functions into the general atlas::gl namespace.
    - Adds the OpenGL error callback to be used with modern OpenGL contexts. This
      allows for better debugging in combination with the previous functionality.

--- include/atlas/gl/ErrorCheck.hpp
+++ include/atlas/gl/ErrorCheck.hpp
@@ -1,6 +1,28 @@
 /**
  *	\file ErrorCheck.hpp
  *	\brief Defines the OpenGL error handling system.
+ *
+ * Recall that OpenGL is essentially a state-machine. As a result,
+ * whenever an error is triggered, the error flag is added to a queue,
+ * which can then be checked until all the errors have been retrieved.
+ * This class (and function) provide a mechanism for doing this 
+ * automatically. Errors are retrieved from OpenGL and then 
+ * printed out using the Log provided by Atlas. For convenience,
+ * use the provided macro.
+ * 
+ * \note
+ * Modern OpenGL (4.3 and above) provides a new mechanism for retrieving
+ * errors using an error callback function. The reason why this isn't
+ * used in Atlas is due to the platform support. Not all Linux 
+ * and Apple machines have access to OpenGL 4.3 so this functionality
+ * would be lost on them. At the same time, adding the code
+ * necessary for the callback would just add extra over-head and hence
+ * it is not implemented.
+ * 
+ * \warning
+ * Atlas does <b> not </b> provide support for OpenGL 2. As such,
+ * any errors generated from this API will not be parsed by this
+ * function. Please use OpenGL 3 and above instead.
  */
 
 #ifndef ATLAS_INCLUDE_ATLAS_GL_ERROR_CHECK_HPP
@@ -16,48 +38,22 @@ namespace atlas
     namespace gl
     {
         /**
-         * \class ErrorCheck
-         * \brief Provides a way for checking OpenGL errors.
-         * 
-         * Recall that OpenGL is essentially a state-machine. As a result,
-         * whenever an error is triggered, the error flag is added to a queue,
-         * which can then be checked until all the errors have been retrieved.
-         * This class (and function) provide a mechanism for doing this 
-         * automatically. Errors are retrieved from OpenGL and then 
-         * printed out using the Log provided by Atlas. For convenience,
-         * use the provided macro.
-         * 
-         * \note
-         * Modern OpenGL (4.3 and above) provides a new mechanism for retrieving
-         * errors using an error callback function. The reason why this isn't
-         * used in Atlas is due to the platform support. Not all Linux 
-         * and Apple machines have access to OpenGL 4.3 so this functionality
-         * would be lost on them. At the same time, adding the code
-         * necessary for the callback would just add extra over-head and hence
-         * it is not implemented.
-         * 
-         * \warning
-         * Atlas does <b> not </b> provide support for OpenGL 2. As such,
-         * any errors generated from this API will not be parsed by this
-         * function. Please use OpenGL 3 and above instead.
+         * Loops through the OpenGL queue retrieving all of the errors
+         * and prints them to the Log.
          */
-        class ErrorCheck
-        {
-        public:
-            /**
-             * Loops through the OpenGL queue retrieving all of the errors
-             * and prints them to the Log.
-             */
-            static void checkGLErrors();
+        void checkGLErrors();
 
-            /**
-             *	Loops through the OpenGL error queue and cleans it out.
-             *	
-             *	\warning
-             *	This function does not print errors! Use with care.
-             */
-            static void clearGLErrors();
-        };
+        /**
+         *	Loops through the OpenGL error queue and cleans it out.
+         *	
+         *	\warning
+         *	This function does not print errors! Use with care.
+         */
+        void clearGLErrors();
+
+        void GLAPIENTRY openGLErrorCallback(GLenum source, GLenum type,
+            GLuint id, GLenum severity, GLsizei length, const GLchar* message,
+            const void* userParam);
     }
 }
 
@@ -67,7 +63,7 @@ namespace atlas
  * all of the namespaces.
  */
 #ifdef ATLAS_DEBUG
-#define GL_ERROR_CHECK() atlas::gl::ErrorCheck::checkGLErrors()
+#define GL_ERROR_CHECK() atlas::gl::checkGLErrors()
 #else
 #define GL_ERROR_CHECK()
 #endif
@@ -79,6 +75,6 @@ namespace atlas
  *	\warning
  *	This will not print errors!
  */
-#define GL_CLEAR_ERRORS() atlas::gl::ErrorCheck::clearGLErrors()
+#define GL_CLEAR_ERRORS() atlas::gl::clearGLErrors()
 
 #endif
--- source/atlas/gl/ErrorCheck.cpp
+++ source/atlas/gl/ErrorCheck.cpp
@@ -6,7 +6,7 @@ namespace atlas
 {
     namespace gl
     {
-        void ErrorCheck::checkGLErrors()
+        void checkGLErrors()
         {
             GLenum err = GL_NO_ERROR;
             while ((err = glGetError()) != GL_NO_ERROR)
@@ -43,11 +43,106 @@ namespace atlas
             }
         }
 
-        void ErrorCheck::clearGLErrors()
+        void clearGLErrors()
         {
             GLenum err = GL_NO_ERROR;
             while ((err = glGetError()) != GL_NO_ERROR)
             { }
+        }
+
+        void openGLErrorCallback(GLenum source, GLenum type, GLuint id,
+            GLenum severity, GLsizei length, const GLchar* message,
+            const void* userParam)
+        {
+            std::string errorOrigin;
+            switch (source)
+            {
+            case GL_DEBUG_SOURCE_API:
+                errorOrigin = "OpenGL API";
+                break;
+
+            case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+                errorOrigin = "window system";
+                break;
+
+            case GL_DEBUG_SOURCE_SHADER_COMPILER:
+                errorOrigin = "shader compiler";
+                break;
+
+            case GL_DEBUG_SOURCE_THIRD_PARTY:
+                errorOrigin = "third party";
+                break;
+
+            case GL_DEBUG_SOURCE_APPLICATION:
+                errorOrigin = "user of application";
+                break;
+
+            case GL_DEBUG_SOURCE_OTHER:
+                errorOrigin = "other";
+                break;
+            }
+
+            std::string errorType;
+            switch (type)
+            {
+            case GL_DEBUG_TYPE_ERROR:
+                errorType = "error";
+                break;
+
+            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+                errorType = "deprecated behaviour";
+                break;
+
+            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+                errorType = "undefined behaviour";
+                break;
+
+            case GL_DEBUG_TYPE_PORTABILITY:
+                errorType = "portability";
+                break;
+
+            case GL_DEBUG_TYPE_PERFORMANCE:
+                errorType = "performance";
+                break;
+
+            case GL_DEBUG_TYPE_MARKER:
+                errorType = "marker";
+                break;
+
+            case GL_DEBUG_TYPE_PUSH_GROUP:
+                errorType = "push group";
+                break;
+
+            case GL_DEBUG_TYPE_POP_GROUP:
+                errorType = "pop group";
+                break;
+
+            case GL_DEBUG_TYPE_OTHER:
+                errorType = "other";
+                break;
+            }
+
+            std::string errorMessage = "[" + errorType + "] : [" +
+                errorOrigin + "] : (" + std::to_string(id) + "): " + message;
+
+            switch (severity)
+            {
+            case GL_DEBUG_SEVERITY_NOTIFICATION:
+                INFO_LOG("OpenGL notification: " + errorMessage);
+                break;
+
+            case GL_DEBUG_SEVERITY_LOW:
+                WARN_LOG("OpenGL warning: " + errorMessage);
+                break;
+
+            case GL_DEBUG_SEVERITY_MEDIUM:
+                ERROR_LOG("OpenGL error: " + errorMessage);
+                break;
+
+            case GL_DEBUG_SEVERITY_HIGH:
+                CRITICAL_LOG("OpenGL critical error: " + errorMessage);
+                break;
+            }
         }
     }
 }
