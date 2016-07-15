76922cd54c5c92b80fef2fc024d73ba1928111ca
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-07-15 13:12:03 -0700

    [brief] Introdues ImGUI to Atlas.
    [detailed]
    - This will be used to provide a simple, immediate-mode GUI to all Atlas
      applications.

---
+++ include/atlas/core/ImGUI.hpp
@@ -0,0 +1,23 @@
+/**
+ * \file ImGUI.hpp
+ * \brief A header wrapper for the imgui header.
+ * 
+ * \note Use
+ * \code
+ * #include <atlas/core/ImGUI.hpp>
+ * \endcode
+ * instead of using
+ * \code
+ * #include <imgui/imgui.h>
+ * \endcode
+ * to prevet errors when/if the header changes.
+ */
+
+#ifndef ATLAS_INCLUDE_ATLAS_CORE_IMGUI_HPP
+#define ATLAS_INCLUDE_ATLAS_CORE_IMGUI_HPP
+
+#pragma once
+
+#include <imgui/imgui.h>
+
+#endif
