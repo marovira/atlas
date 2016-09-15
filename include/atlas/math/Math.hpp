5569fb75a4f62804b08aea3ec1940b5c1181fb88
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-09-14 23:40:35 -0700

    [brief] Updates documentation for math module.
    [detailed]

--- include/atlas/math/Math.hpp
+++ include/atlas/math/Math.hpp
@@ -28,21 +28,82 @@ namespace atlas
 
         struct Ray;
 
+        /**
+         * \typedef Vector2
+         * Defines an alias for a 2D vector.
+         */
         using Vector2 = glm::vec2;
+
+        /**
+         * \typedef Vector
+         * Defines an alias for a 3D vector.
+         */
         using Vector = glm::vec3;
+
+        /**
+         * \typedef Vector4
+         * Defines an alias for a 4D vector.
+         */
         using Vector4 = glm::vec4;
 
+        /**
+         * \typedef Point2
+         * Defines an alias for a 2D point.
+         */
         using Point2 = Vector2;
+
+        /**
+         * \typedef Point
+         * Defines an alias for a 3D point.
+         */
         using Point = Vector;
+
+        /**
+         * \typedef Point4
+         * Defines an alias for a 4D point.
+         */
         using Point4 = Vector4;
 
+        /**
+         * \typedef Normal2
+         * Defines an alias for a 2D normal.
+         */
         using Normal2 = Vector2;
+
+        /**
+         * \typedef Normal
+         * Defines an alias for a 3D normal.
+         */
         using Normal = Vector;
+
+        /**
+         * \typedef Normal4
+         * Defines an alias for a 4D normal.
+         */
         using Normal4 = Vector4;
 
-        using Matrix4 = glm::mat4;
+        /**
+         * \typedef Matrix2
+         * Defines an alias for a 2x2 matrix.
+         */
+        using Matrix2 = glm::mat2;
+
+        /**
+         * \typedef Matrix3
+         * Defines an alias for a 3x3 matrix.
+         */
         using Matrix3 = glm::mat3;
 
+        /**
+         * \typedef Matrix4
+         * Defines an alias for a 4x4 matrix.
+         */
+        using Matrix4 = glm::mat4;
+
+        /**
+         * \typedef Quaternion
+         * Defines an alias for a quaternion.
+         */
         using Quaternion = glm::quat;
     }
 }
