bd9125b6556ebc231cba2f43b8ba36ca7ccf0ffa
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2016-10-18 13:09:01 -0700

    [brief] Expands constants to add min/max and adds documentation.
    [detailed]
    - Not all types (see integral) have a representation of infinity. That said, it is
      still useful to be able to access these values in a straight-forward way. To do
      this, two new functions have been added: min and max.
    - Adds proper documentation for all functions.

--- include/atlas/core/Constants.hpp
+++ include/atlas/core/Constants.hpp
@@ -17,6 +17,7 @@ namespace atlas
     {
         /**
          * Returns the defined epsilon value for that data type.
+         * 
          * @tparam The numerical data type.
          */
         template <typename GenType = float>
@@ -26,8 +27,12 @@ namespace atlas
         }
 
         /**
-         * Returns the "infinity" (maximum value) that the specified data type
-         * can represent.
+         * Returns the value of positive infinity.
+         * \note
+         * This function is disabled for types that do not have a
+         * valid representation of positive infinity. For the maximum value
+         * that a type can hold, see max().
+         * 
          * @tparam The numerical data type.
          */
         template <typename GenType = float>
@@ -38,12 +43,45 @@ namespace atlas
             return std::numeric_limits<GenType>::infinity();
         }
 
+        /**
+         * Returns the value of negative infinity.
+         * \note
+         * This function is disabled for types tat do not have a valid
+         * representation of negative infinity. For the minimum value that a
+         * type can hold, see min().
+         * 
+         * @tparam The numerical data type.
+         */
         template <typename GenType = float>
         constexpr typename std::enable_if<
             std::numeric_limits<GenType>::has_infinity, GenType>::type
             negInfinity()
         {
             return GenType(-1) * infinity<GenType>();
+        }
+
+        /**
+         * Returns the maximum value that the type can hold. Not to be confused
+         * with infinity. For further details, see infinity().
+         * 
+         * @tparam The numerical data type.
+         */
+        template <typename GenType>
+        constexpr GenType max()
+        {
+            retun std::numeric_limits<GenType>::max();
+        }
+
+        /**
+         * Returns the minimum value that the type can hold. Not to be confused
+         * with negative infinity. For further details, see negInfinity().
+         * 
+         * @tparam The numerical data type.
+         */
+        template <typename GenType>
+        constexpr GenType min()
+        {
+            return std::numeric_limits<GenType>::lowest();
         }
     }
 }
