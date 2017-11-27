4a96391b4e8664803e619f06a617d5957bccf35e
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2017-11-27 01:39:14 -0800

    [brief] Preliminary Framebuffer implementation.
    [detailed]
    - Just provides code for simple functions. More stuff will be added
      later.

---
+++ include/atlas/gl/FrameBuffer.hpp
@@ -0,0 +1,46 @@
+/**
+ * \file FrameBuffer.hpp
+ * \brief Defines a wrapper for OpenGL framebuffer objects.
+ */
+
+#ifndef ATLAS_INCLUDE_ATLAS_GL_FRAME_BUFFER_HPP
+#define ATLAS_INCLUDE_ATLAS_GL_FRAME_BUFFER_HPP
+
+#pragma once
+
+#include "GL.hpp"
+
+#include <memory>
+
+namespace atlas
+{
+    namespace gl
+    {
+        class FrameBuffer : public Object
+        {
+        public:
+            FrameBuffer();
+
+            FrameBuffer(FrameBuffer&& rhs);
+
+            ~FrameBuffer();
+
+            FrameBuffer& operator=(FrameBuffer&& rhs);
+
+            void bindBuffer();
+
+            void unBindBuffer();
+
+            void bufferStorage(GLenum internalFormat, GLsizei width, 
+                GLsizei height);
+
+            void getParameter(GLenum param, GLint* value);
+
+        private:
+            struct FrameBufferImpl;
+            std::unique_ptr<FrameBufferImpl> mImpl;
+        };
+    }
+}
+
+#endif
---
+++ source/atlas/gl/FrameBuffer.cpp
@@ -0,0 +1,67 @@
+#include "atlas/gl/FrameBuffer.hpp"
+#include "atlas/gl/ErrorCheck.hpp"
+#include "atlas/core/Log.hpp"
+
+namespace atlas
+{
+    namespace gl
+    {
+        struct FrameBuffer::FrameBufferImpl
+        {
+            FrameBufferImpl() :
+                handle(0)
+            { }
+
+            FrameBufferImpl(FrameBufferImpl const& impl) = default;
+            ~FrameBufferImpl() = default;
+
+            GLuint handle;
+        };
+
+        FrameBuffer::FrameBuffer() :
+            mImpl(std::make_unique<FrameBufferImpl>())
+        {
+            glGenFramebuffers(1, &mImpl->handle);
+        }
+
+        FrameBuffer::FrameBuffer(FrameBuffer&& rhs) :
+            mImpl(std::make_unique<FrameBufferImpl>(*rhs.mImpl))
+        {
+            rhs.mImpl->handle = 0;
+        }
+
+        FrameBuffer& FrameBuffer::operator=(FrameBuffer&& rhs)
+        {
+            *mImpl = *rhs.mImpl;
+            rhs.mImpl->handle = 0;
+
+            return *this;
+        }
+
+        FrameBuffer::~FrameBuffer()
+        {
+            glDeleteFramebuffers(1, &mImpl->handle);
+        }
+
+        void FrameBuffer::bindBuffer()
+        {
+            glBindFramebuffer(GL_FRAMEBUFFER, mImpl->handle);
+        }
+
+        void FrameBuffer::unBindBuffer()
+        {
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        }
+
+        void FrameBuffer::bufferStorage(GLenum internalFormat, GLsizei width,
+            GLsizei height)
+        {
+            glRenderbufferStorage(GL_FRAMEBUFFER, internalFormat, width, height);
+        }
+
+        void FrameBuffer::getParameter(GLenum param, GLint* value)
+        {
+            glGetRenderbufferParameteriv(GL_FRAMEBUFFER, param, value);
+        }
+    }
+}
