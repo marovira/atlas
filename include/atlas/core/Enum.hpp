3bac6f286f7c137a58912cfbf39fded7c857cda0
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-12-05 18:11:04 -0800

    [brief] Adds enum header.
    [detailed]
    - Function to convert enums to underlying types is now correctly enabled
      only on enums.

---
+++ include/atlas/core/Enum.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <type_traits>
+
+namespace atlas
+{
+    namespace core
+    {
+        template <typename T,
+        typename = std::enable_if<std::is_enum<T>::value>>
+        constexpr typename std::underlying_type<T>::type
+            enumToUnderlyingType(T value)
+        {
+            using underType = typename std::underlying_type<T>::type;
+            return static_cast<underType>(value);
+        }
+    }
+}
