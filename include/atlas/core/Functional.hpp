ca244349c1d71c76e4aeee33c2b898f936098728
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-12-06 15:11:31 -0800

    [brief] Adds functional header.
    [detailed]
    - Currently contains only an overload of iota that allows arbitrary step
      sizes.

---
+++ include/atlas/core/Functional.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace atlas::core
+{
+    template <typename ForwardIt, typename T>
+    void iota(ForwardIt first, ForwardIt last, T value, T step)
+    {
+        while (first != last)
+        {
+            *first++ = value;
+            value += step;
+        }
+    }
+}
