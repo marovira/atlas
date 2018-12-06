b6c06245b1f209c35ee130848081f20116677148
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-12-06 15:12:47 -0800

    [brief] Adds missing extra lien at end of files.
    [detailed]

--- include/atlas/core/Platform.hpp
+++ include/atlas/core/Platform.hpp
@@ -1,39 +1,34 @@
 #pragma once
 
-namespace atlas
+namespace atlas::core
 {
-    namespace core
+    enum class Platform
     {
-        enum class Platform
-        {
-            Windows = 0,
-            Linux
-        };
+        Windows = 0,
+        Linux
+    };
 
-        enum class BuildType
-        {
-            Debug = 0,
-            Release
-        };
+    enum class BuildType
+    {
+        Debug = 0,
+        Release
+    };
 
 #if defined(_WIN32) || defined(_WIN64)
 #define ATLAS_PLATFORM_WINDOWS
-        static constexpr Platform CurrentPlatform{ Platform::Windows };
+    static constexpr Platform CurrentPlatform{ Platform::Windows };
 #elif defined(__APPLE__)
 #error "Support for Apple is deprecated"
 #elif defined(__linux__)
 #define ATLAS_PLATFORM_LINUX
-            static constexpr Platform CurrentPlatform{ Platform::Linux };
+        static constexpr Platform CurrentPlatform{ Platform::Linux };
 #endif
 
 #if defined(_DEBUG) || defined(DEBUG) || defined(__DEBUG__)
 #define ATLAS_BUILD_DEBUG
-            static constexpr  BuildType CurrentBuild{ BuildType::Debug };
+        static constexpr  BuildType CurrentBuild{ BuildType::Debug };
 #else
 #define ATLAS_BUILD_RELEASE
-            static constexpr  BuildType CurrentBuild{ BuildType::Release };
+        static constexpr  BuildType CurrentBuild{ BuildType::Release };
 #endif
-
-    }
 }
-
