905a167bb8a96d47dd05252c46274f671fb1f0d9
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-12-05 17:57:31 -0800

    [brief] Starts the core module.
    [detailed]
    - Modernizes macros by also adding constexpr variables. These can be
      used on templates for compile-time optimizations when needed.
    - Better defines macro names.
    - Replaces version macros with constexpr variables.

---
+++ include/atlas/core/Platform.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace atlas
+{
+    namespace core
+    {
+        enum class Platform
+        {
+            Windows = 0,
+            Linux
+        };
+
+        enum class BuildType
+        {
+            Debug = 0,
+            Release
+        };
+
+#if defined(_WIN32) || defined(_WIN64)
+#define ATLAS_PLATFORM_WINDOWS
+        static constexpr Platform CurrentPlatform{ Platform::Windows };
+#elif defined(__APPLE__)
+#error "Support for Apple is deprecated"
+#elif defined(__linux__)
+#define ATLAS_PLATFORM_LINUX
+            static constexpr Platform CurrentPlatform{ Platform::Linux };
+#endif
+
+#if defined(_DEBUG) || defined(DEBUG) || defined(__DEBUG__)
+#define ATLAS_BUILD_DEBUG
+            static constexpr  BuildType CurrentBuild{ BuildType::Debug };
+#else
+#define ATLAS_BUILD_RELEASE
+            static constexpr  BuildType CurrentBuild{ BuildType::Release };
+#endif
+
+    }
+}
+
