fe1eb25c7f186a5596301d3d129cb5ed810cd9cd
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-12-13 11:54:14 -0800

    [brief] Adds Ray class.
    [detailed]

---
+++ include/atlas/math/Ray.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Math.hpp"
+
+namespace atlas::math
+{
+    template <typename T>
+    struct Ray
+    {
+        Ray() :
+            o{ 0.0f },
+            d{ 0.0f }
+        { }
+
+        Ray(T const& origin, T const& dir) :
+            o{ origin },
+            d{ dir }
+        { }
+
+        T operator()(float t) const
+        {
+            return o + (t * d);
+        }
+
+        bool operator==(Ray const& rhs)
+        {
+            return (o == rhs.o) && (d == rhs.d);
+        }
+
+        bool operator!=(Ray const& rhs)
+        {
+            return !(*this == rhs);
+        }
+
+        T o;
+        T d;
+    };
+}
