458b30f5e102ce574a5bc380694fd0e5a02d982d
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-12-12 18:19:18 -0800

    [brief] Adds math module.
    [detailed]
    - Adds dependency to FMT for printing on core.
    - Adds dependency to GLM on math.
    - Adds Coordinates header.
    - Adds initial tests for coordinates. Currently failing.

---
+++ include/atlas/math/Coordinates.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Math.hpp"
+
+namespace atlas::math
+{
+    Point cartesianToSpherical(Point const& p)
+    {
+        float r = glm::length(p);
+        float theta = glm::atan(p.y / p.x);
+        float phi = glm::acos(p.z / r);
+        return { r, theta, phi };
+    }
+
+    Point sphericalToCartesian(Point const& p)
+    {
+        float x = p.r * glm::cos(p.y) * glm::sin(p.z);
+        float y = p.r * glm::sin(p.y) * glm::sin(p.z);
+        float z = p.r * glm::cos(p.z);
+        return { x, y, z };
+    }
+
+    Point cartesianToCylindrical(Point const& p)
+    {
+        float r = glm::length(Point2{ p });
+        float theta = glm::atan(p.y / p.x);
+        return { r, theta, p.z };
+    }
+
+    Point cylindricalToCartesian(Point const& p)
+    {
+        float x = p.x * glm::cos(p.y);
+        float y = p.x * glm::sin(p.y);
+        return { x, y, p.z };
+    }
+
+    Point2 cartesianToPolar(Point2 const& p)
+    {
+        float r = glm::length(p);
+        float theta = glm::atan(p.y / p.x);
+        return { r, theta };
+    }
+
+    Point2 polarToCartesian(Point2 const& p)
+    {
+        float x = p.r * glm::cos(p.y);
+        float y = p.r * glm::sin(p.y);
+        return { x, y };
+    }
+}
