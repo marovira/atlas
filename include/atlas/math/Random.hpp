340a605475cb0fa1314b6fe392484dc3d3bbf379
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-12-13 12:16:33 -0800

    [brief] Adds a random number generator.
    [detailed]

---
+++ include/atlas/math/Random.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <random>
+#include <limits>
+#include <type_traits>
+
+namespace atlas::math
+{
+    template <typename T, typename Engine,
+        typename std::enable_if<std::is_arithmetic<T>::type>>
+    class Random
+    {
+    public:
+        Random() = default;
+
+        Random(typename Engine::result_type seed) :
+            mEngine{ seed }
+        { }
+
+        T getRandomRange(T min, T max) const
+        {
+            std::uniform_real_distribution<T> dist(min, max);
+            return dist(mEngine);
+        }
+
+        T getRandomMax() const
+        {
+            return getRandomRange(static_cast<T>(0),
+                std::numeric_limits<T>::max());
+        }
+
+        T getRandomOne() const
+        {
+            return getRandomRange(static_cast<T>(0), static_cast<T>(1));
+        }
+
+    private:
+        Engine mEngine{ std::random_device{}() };
+    };
+}
