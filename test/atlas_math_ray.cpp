3a7bd113d147f6e46da89563a4ba4afff4f36cdc
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-01-23 18:21:23 -0800

    [brief] Finishes ray unit tests.
    [detailed]

--- test/atlas_math_ray.cpp
+++ test/atlas_math_ray.cpp
@@ -9,6 +9,15 @@ TEST_CASE("Checking Ray class", "[math]")
     Ray<Point> r;
     Ray<Point> r1{ Point{0.0f}, Point{0.0f} };
     REQUIRE((r == r1) == true);
+    REQUIRE((r != r1) == false);
 
+    r1.o = Point{ 1.0f };
+    r1.d = Point{ 1.0f };
 
+    REQUIRE((r == r1) == false);
+    REQUIRE((r != r1) == true);
+
+    Point p{ 2.0f };
+    auto val = r1(1.0f);
+    REQUIRE((val == p) == true);
 }
