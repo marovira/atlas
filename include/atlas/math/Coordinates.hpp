3f97deb0ad137d845ef46a1757a54d6e96254ab5
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-03-02 21:15:05 -0800

    [brief] Reformats math module to current coding standards.
    [detailed]

--- include/atlas/math/Coordinates.hpp
+++ include/atlas/math/Coordinates.hpp
@@ -6,10 +6,10 @@ namespace atlas::math
 {
     Point cartesianToSpherical(Point const& p)
     {
-        float r = glm::length(p);
+        float r     = glm::length(p);
         float theta = glm::atan(p.y / p.x);
-        float phi = glm::acos(p.z / r);
-        return { r, theta, phi };
+        float phi   = glm::acos(p.z / r);
+        return {r, theta, phi};
     }
 
     Point sphericalToCartesian(Point const& p)
@@ -17,34 +17,34 @@ namespace atlas::math
         float x = p.r * glm::cos(p.y) * glm::sin(p.z);
         float y = p.r * glm::sin(p.y) * glm::sin(p.z);
         float z = p.r * glm::cos(p.z);
-        return { x, y, z };
+        return {x, y, z};
     }
 
     Point cartesianToCylindrical(Point const& p)
     {
-        float r = glm::length(Point2{ p });
+        float r     = glm::length(Point2{p});
         float theta = glm::atan(p.y / p.x);
-        return { r, theta, p.z };
+        return {r, theta, p.z};
     }
 
     Point cylindricalToCartesian(Point const& p)
     {
         float x = p.x * glm::cos(p.y);
         float y = p.x * glm::sin(p.y);
-        return { x, y, p.z };
+        return {x, y, p.z};
     }
 
     Point2 cartesianToPolar(Point2 const& p)
     {
-        float r = glm::length(p);
+        float r     = glm::length(p);
         float theta = glm::atan(p.y / p.x);
-        return { r, theta };
+        return {r, theta};
     }
 
     Point2 polarToCartesian(Point2 const& p)
     {
         float x = p.r * glm::cos(p.y);
         float y = p.r * glm::sin(p.y);
-        return { x, y };
+        return {x, y};
     }
-}
+} // namespace atlas::math
