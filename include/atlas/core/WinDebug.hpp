7704f660bfd400a2127dae9236277b59d0dc0d1a
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-04-07 16:29:45 -0700

    [brief] Fixes typo on Windows macros.
    [detailed]
    - Forgot leading underscore on both.
    - Applies new formatting rules.

--- include/atlas/core/WinDebug.hpp
+++ include/atlas/core/WinDebug.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#if defined(WIN32) || defined(WIN64)
+#if defined(_WIN32) || defined(_WIN64)
 
-#    include <fmt/printf.h>
+#include <fmt/printf.h>
 
-#    define WIN32_LEAN_AND_MEAN
-#    include <windows.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
-#    include <cstring>
+#include <cstring>
 
 namespace atlas::core
 {
