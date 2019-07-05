69cc9ccb4693168e668540a0fd1937f75235e3c8
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-07-04 21:51:34 -0700

    [brief] Adds FMT wrapper header.
    [detailed]
    - This is well an truly a hack. Due to the fact that MSVC shoves
      warnings everywhere, it gets a bit annoying to see the ones belonging
      to external code. As a result, a wrapper header that removes these
      warnings (on Windows only) has been added.

---
+++ include/atlas/core/FMT.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Platform.hpp"
+
+#if defined(ATLAS_PLATFORM_WINDOWS)
+#pragma warning(push)
+#pragma warning(disable : 26451)
+#pragma warning(disable : 6387)
+#pragma warning(disable : 26498)
+#pragma warning(disable : 26495)
+#endif
+
+#include <fmt/printf.h>
+
+#if defined(ATLAS_PLATFORM_WINDOWS)
+#pragma warning(pop)
+#endif
