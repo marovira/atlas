ddfd1eaa036660d106f146115fe466ce8dda0996
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-08-04 22:01:07 -0700

    [brief] Adds the tests for library reloading.
    [detailed]

---
+++ test/atlas_hlr_loadlibrary.cpp
@@ -0,0 +1,73 @@
+#include <atlas/hlr/LoadLibrary.hpp>
+
+#include <atlas/core/FMT.hpp>
+#include <atlas/core/Platform.hpp>
+
+#if defined(ATLAS_BUILD_DEBUG)
+#    include "TestLibraryDebugPath.hpp"
+#else
+#    include "TestLibraryReleasePath.hpp"
+#endif
+
+#include <catch2/catch.hpp>
+
+using namespace atlas;
+
+TEST_CASE("Checking library loading", "[hlr]")
+{
+    const std::string libraryPath{TestLibraryPath};
+    void* handle = hlr::loadLibraryHandle(libraryPath);
+
+    REQUIRE(handle != nullptr);
+
+    hlr::unloadLibraryHandle(handle);
+}
+
+typedef int (*SumFn)(int, int);
+
+TEST_CASE("Checking raw function loading", "[hlr]")
+{
+    const std::string libraryPath{TestLibraryPath};
+    void* handle = hlr::loadLibraryHandle(libraryPath);
+
+    REQUIRE(handle != nullptr);
+
+    void* sumFn = hlr::loadRawFunctionPtr(handle, "add");
+    REQUIRE(sumFn != nullptr);
+
+    SumFn fn = reinterpret_cast<SumFn>(sumFn);
+    int r    = fn(1, 2);
+    REQUIRE(r == 3);
+
+    hlr::unloadLibraryHandle(handle);
+}
+
+TEST_CASE("Checking template function pointer loading", "[hlr]")
+{
+    const std::string libraryPath{TestLibraryPath};
+    void* handle = hlr::loadLibraryHandle(libraryPath);
+
+    REQUIRE(handle != nullptr);
+
+    auto fn = hlr::loadFunctionPtr<SumFn>(handle, "add");
+
+    int r = fn(1, 2);
+    REQUIRE(r == 3);
+
+    hlr::unloadLibraryHandle(handle);
+}
+
+TEST_CASE("Checking template function loading", "[hlr]")
+{
+    const std::string libraryPath{TestLibraryPath};
+    void* handle = hlr::loadLibraryHandle(libraryPath);
+
+    REQUIRE(handle != nullptr);
+
+    auto fn = hlr::loadFunction<SumFn, int(int, int)>(handle, "add");
+
+    int r = fn(1, 2);
+    REQUIRE(r == 3);
+
+    hlr::unloadLibraryHandle(handle);
+}
