a452e5530cf1df1b559c5891c23c777051e48cda
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-08-04 22:00:30 -0700

    [brief] Adds the functions to load libraries.
    [detailed]
    - Note that these are only the windows versions of the functions. We are
      missing the linux versions.

---
+++ source/atlas/hlr/LoadLibrary.cpp
@@ -0,0 +1,55 @@
+#include "atlas/hlr/LoadLibrary.hpp"
+
+#include <atlas/core/FMT.hpp>
+#include <atlas/core/Platform.hpp>
+
+#if defined(ATLAS_PLATFORM_WINDOWS)
+#    define WIN32_LEAN_AND_MEAN
+#    include <windows.h>
+#endif
+
+namespace atlas::hlr
+{
+#if defined(ATLAS_PLATFORM_WINDOWS)
+    void* loadLibraryHandle(std::string const& libraryName)
+    {
+        void* handle = LoadLibrary(libraryName.c_str());
+        if (handle == nullptr)
+        {
+            auto errorCode = GetLastError();
+            fmt::print(stderr, "error: LoadLibrary returned error code {}\n",
+                       errorCode);
+        }
+
+        return handle;
+    }
+
+    void* loadRawFunctionPtr(void* handle, std::string const& functionName)
+    {
+        if (handle == nullptr)
+        {
+            fmt::print(stderr, "error: null library handle\n");
+            return nullptr;
+        }
+
+        HMODULE h = reinterpret_cast<HMODULE>(handle);
+
+        FARPROC procAddress = GetProcAddress(h, functionName.c_str());
+        if (procAddress == nullptr)
+        {
+            auto errorCode = GetLastError();
+            fmt::print(stderr, "error: GetProcAddress returned error code {}\n",
+                       errorCode);
+        }
+
+        return procAddress;
+    }
+
+    void unloadLibraryHandle(void* handle)
+    {
+        HMODULE h = reinterpret_cast<HMODULE>(handle);
+        FreeLibrary(h);
+    }
+#else
+#endif
+} // namespace atlas::hlr
