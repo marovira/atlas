a7bcd73dc2ee91ec173dda31b3981428355bab58
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-08-07 22:11:49 -0700

    [brief] Refactors LoadObjFile.
    [detailed]
    - The issue we were having was falling into the same pitfall as before:
      try to guess what the user wants to do/load. Since this is exactly
      what we're trying to move away from, the entire notion of
      post-processing the output from tinyobj has been scrapped. Instead,
      the results are returned as-is. The user can then perform whatever
      processing is required for their purposes.

--- include/atlas/utils/LoadObjFile.hpp
+++ include/atlas/utils/LoadObjFile.hpp
@@ -10,43 +10,12 @@
 
 namespace atlas::utils
 {
-    struct Face
+    struct ObjMesh
     {
-        Face() = default;
-
-        Face(std::size_t st, std::size_t sz, int id) :
-            start{st},
-            size{sz},
-            materialId{id}
-        {}
-
-        std::size_t start{};
-        std::size_t size{};
-        int materialId{};
-    };
-
-    struct Shape
-    {
-        Shape() = default;
-
-        Shape(std::size_t start, std::vector<Face> const& f) :
-            dataStart{start},
-            faces{f}
-        {}
-
-        std::size_t dataStart{};
-        std::vector<Face> faces;
-    };
-
-    struct Mesh
-    {
-        std::vector<math::Point> vertices;
-        std::vector<math::Normal> normals;
-        std::vector<math::Point2> texCoords;
-        std::vector<std::uint32_t> indices;
-        std::vector<Shape> shapes;
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
         std::vector<tinyobj::material_t> materials;
     };
 
-    std::optional<Mesh> loadObjMesh(std::string const& fileName);
+    std::optional<ObjMesh> loadObjMesh(std::string const& filename);
 } // namespace atlas::utils
