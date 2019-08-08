b291998db5d39123e7683813e5c98622e8c6c1c8
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-08-07 22:14:11 -0700

    [brief] Removes obj tester.
    [detailed]
    - The function is now a thin wrapper for tinyobj, so there's no real
      purpose in testing it.
    - Removes all other framework built for the tester.

--- download-obj.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Downloading test data..."
-mkdir ./test/data/obj
-curl https://casual-effects.com/g3d/data10/common/model/cube/cube.zip -o ./test/data/obj/cube.zip
-curl https://casual-effects.com/g3d/data10/common/model/crytek_sponza/sponza.zip -o ./test/data/obj/sponza.zip
-curl https://casual-effects.com/g3d/data10/common/model/teapot/teapot.zip -o ./test/data/obj/teapot.zip
-echo "Test data download complete"
