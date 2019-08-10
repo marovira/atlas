b0f5df737f2a861a94d81c3670ea2fbea5bbb6ea
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-08-10 12:44:01 -0700

    [brief] Adds the OpenGL error callback.
    [detailed]
    - This function was missing and is really useful to have, so it's been
      restored.
    - It uses enums as flags instead of macros (or integral constants), so
      it will require the use of the enum operators we defined previously.
    - It does have a global static variable. In all fairness, I couldn't
      come up with a way of bypassing the function boundaries without it. If
      I come up with a better solution later on, I will change this.

---
+++ include/atlas/glx/ErrorCallback.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <GL/gl3w.h>
+#include <atlas/core/Enum.hpp>
+
+namespace atlas::glx
+{
+    enum class ErrorSource : unsigned short
+    {
+        API            = 0x01,
+        WindowSystem   = 0x02,
+        ShaderCompiler = 0x04,
+        ThirdParty     = 0x08,
+        Application    = 0x10,
+        Other          = 0x20,
+        All            = 0x3F,
+        None           = 0x00
+    };
+
+    enum class ErrorType : unsigned short
+    {
+        Error               = 0x001,
+        DeprecatedBehaviour = 0x002,
+        UndefinedBehaviour  = 0x004,
+        Portability         = 0x008,
+        Performance         = 0x010,
+        Marker              = 0x020,
+        PushGroup           = 0x040,
+        PopGroup            = 0x080,
+        Other               = 0x100,
+        All                 = 0x1FF,
+        None                = 0x000
+    };
+
+    enum class ErrorSeverity : unsigned short
+    {
+        Notification = 0x1,
+        Low          = 0x2,
+        Medium       = 0x4,
+        High         = 0x8,
+        All          = 0xF,
+        None         = 0x0
+    };
+
+    void initializeGLCallback(ErrorSource source     = ErrorSource::All,
+                              ErrorType type         = ErrorType::All,
+                              ErrorSeverity severity = ErrorSeverity::All);
+
+} // namespace atlas::glx
---
+++ source/atlas/glx/ErrorCallback.cpp
@@ -0,0 +1,245 @@
+#include "atlas/glx/ErrorCallback.hpp"
+
+#include <atlas/core/Platform.hpp>
+
+#include <fmt/printf.h>
+
+namespace atlas::glx
+{
+    struct ErrorFilters
+    {
+        ErrorSource source;
+        ErrorType type;
+        ErrorSeverity severity;
+    };
+
+    static ErrorFilters gErrorFilters;
+
+    void APIENTRY openGLErrorCallback(GLenum source, GLenum type, GLuint id,
+                                      GLenum severity, GLsizei length,
+                                      GLchar const* message,
+                                      void const* userParam);
+
+    void initializeGLCallback(ErrorSource source, ErrorType type,
+                              ErrorSeverity severity)
+    {
+#if defined(ATLAS_BUILD_DEBUG)
+        if (glDebugMessageCallback)
+        {
+            fmt::print("OpenGL debug callback available\n");
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+            glDebugMessageCallback(openGLErrorCallback, nullptr);
+            GLuint unusedIds{0};
+            glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0,
+                                  &unusedIds, true);
+
+            gErrorFilters.source   = source;
+            gErrorFilters.type     = type;
+            gErrorFilters.severity = severity;
+        }
+        else
+        {
+            fmt::print("OpenGL debug callback not available\n");
+        }
+#else
+        fmt::print("OpenGL debug callback is only available in debug builds\n");
+#endif
+    }
+
+    using namespace atlas::core;
+
+    template<typename T>
+    bool check(T a, T b)
+    {
+        T c      = a & b;
+        auto res = enumToUnderlyingType(c);
+        return (res != 0);
+    }
+
+    void APIENTRY openGLErrorCallback(GLenum source, GLenum type, GLuint id,
+                                      GLenum severity,
+                                      [[maybe_unused]] GLsizei length,
+                                      GLchar const* message,
+                                      [[maybe_unused]] void const* userParam)
+    {
+        std::string errorOrigin;
+        switch (source)
+        {
+        case GL_DEBUG_SOURCE_API:
+            if (check(gErrorFilters.source, ErrorSource::API))
+            {
+                errorOrigin = "OpenGL API";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+            if (check(gErrorFilters.source, ErrorSource::WindowSystem))
+            {
+                errorOrigin = "window system";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+            if (check(gErrorFilters.source, ErrorSource::ShaderCompiler))
+            {
+                errorOrigin = "shader compiler";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+            if (check(gErrorFilters.source, ErrorSource::ThirdParty))
+            {
+                errorOrigin = "third party";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_SOURCE_APPLICATION:
+            if (check(gErrorFilters.source, ErrorSource::Application))
+            {
+                errorOrigin = "user of application";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_SOURCE_OTHER:
+            if (check(gErrorFilters.source, ErrorSource::Other))
+            {
+                errorOrigin = "other";
+                break;
+            }
+
+            return;
+        }
+
+        std::string errorType;
+        switch (type)
+        {
+        case GL_DEBUG_TYPE_ERROR:
+            if (check(gErrorFilters.type, ErrorType::Error))
+            {
+                errorType = "error";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+            if (check(gErrorFilters.type, ErrorType::DeprecatedBehaviour))
+            {
+                errorType = "deprecated behaviour";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+            if (check(gErrorFilters.type, ErrorType::UndefinedBehaviour))
+            {
+                errorType = "undefined behaviour";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_TYPE_PORTABILITY:
+            if (check(gErrorFilters.type, ErrorType::Portability))
+            {
+                errorType = "portability";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_TYPE_PERFORMANCE:
+            if (check(gErrorFilters.type, ErrorType::Performance))
+            {
+                errorType = "performance";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_TYPE_MARKER:
+            if (check(gErrorFilters.type, ErrorType::Marker))
+            {
+                errorType = "marker";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_TYPE_PUSH_GROUP:
+            if (check(gErrorFilters.type, ErrorType::PushGroup))
+            {
+                errorType = "push group";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_TYPE_POP_GROUP:
+            if (check(gErrorFilters.type, ErrorType::PopGroup))
+            {
+                errorType = "pop group";
+                break;
+            }
+
+            return;
+
+        case GL_DEBUG_TYPE_OTHER:
+            if (check(gErrorFilters.type, ErrorType::Other))
+            {
+                errorType = "other";
+                break;
+            }
+
+            return;
+        }
+
+        std::string errorMessage =
+            fmt::format("{}({}):({}): {}", errorType, errorOrigin, id, message);
+
+        switch (severity)
+        {
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            if (check(gErrorFilters.severity, ErrorSeverity::Notification))
+            {
+                fmt::print(stdout, "OpenGL notification: {}\n", errorMessage);
+            }
+
+            break;
+
+        case GL_DEBUG_SEVERITY_LOW:
+            if (check(gErrorFilters.severity, ErrorSeverity::Low))
+            {
+                fmt::print(stdout, "OpenGL warning: {}\n", errorMessage);
+            }
+
+            break;
+
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            if (check(gErrorFilters.severity, ErrorSeverity::Medium))
+            {
+                fmt::print(stderr, "OpenGL error: {}\n", errorMessage);
+            }
+
+            break;
+
+        case GL_DEBUG_SEVERITY_HIGH:
+            if (check(gErrorFilters.severity, ErrorSeverity::High))
+            {
+                fmt::print(stderr, "OpenGL critical error: {}\n", errorMessage);
+            }
+            break;
+        }
+    }
+} // namespace atlas::glx
