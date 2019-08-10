95b43b2f22312a9255427e3d78d50f57c9404bbd
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-08-10 11:48:54 -0700

    [brief] Adds new operators for enum class types.
    [detailed]
    - This is more of a convenience thing to allow for certain arithmetic to
      be performed on enums for flags.

--- include/atlas/core/Enum.hpp
+++ include/atlas/core/Enum.hpp
@@ -8,7 +8,55 @@ namespace atlas::core
     constexpr typename std::underlying_type<T>::type
     enumToUnderlyingType(T value)
     {
-        using underType = typename std::underlying_type<T>::type;
-        return static_cast<underType>(value);
+        using UnderType = typename std::underlying_type<T>::type;
+        return static_cast<UnderType>(value);
     }
+
+    template<typename T,
+             typename = std::enable_if<
+                 std::is_enum<T>::value &&
+                 std::is_unsigned<std::underlying_type<T>::type>::value>>
+    constexpr T operator~(T a)
+    {
+        using UnderType = typename std::underlying_type<T>::type;
+        UnderType var   = ~enumToUnderlyingType(a);
+        return static_cast<T>(var);
+    }
+
+    template<typename T,
+             typename = std::enable_if<
+                 std::is_enum<T>::value &&
+                 std::is_unsigned<std::underlying_type<T>::type>::value>>
+    constexpr T operator|(T a, T b)
+    {
+        using UnderType = typename std::underlying_type<T>::type;
+        UnderType uA    = enumToUnderlyingType(a);
+        UnderType uB    = enumToUnderlyingType(b);
+        return static_cast<T>(uA | uB);
+    }
+
+    template<typename T,
+             typename = std::enable_if<
+                 std::is_enum<T>::value &&
+                 std::is_unsigned<std::underlying_type<T>::type>::value>>
+    constexpr T operator&(T a, T b)
+    {
+        using UnderType = typename std::underlying_type<T>::type;
+        UnderType uA    = enumToUnderlyingType(a);
+        UnderType uB    = enumToUnderlyingType(b);
+        return static_cast<T>(uA & uB);
+    }
+
+    template<typename T,
+             typename = std::enable_if<
+                 std::is_enum<T>::value &&
+                 std::is_unsigned<std::underlying_type<T>::type>::value>>
+    constexpr T operator^(T a, T b)
+    {
+        using UnderType = typename std::underlying_type<T>::type;
+        UnderType uA    = enumToUnderlyingType(a);
+        UnderType uB    = enumToUnderlyingType(b);
+        return static_cast<T>(uA ^ uB);
+    }
+
 } // namespace atlas::core
--- test/atlas_core_enum.cpp
+++ test/atlas_core_enum.cpp
@@ -2,19 +2,28 @@
 
 #include <catch2/catch.hpp>
 
-enum class Test : int
+enum class TypeTest : int
 {
     A = 0,
     B,
     C
 };
 
+enum class OpTest : unsigned char
+{
+    A = 0x00,
+    B = 0x01,
+    C = 0x10,
+    D = 0x11,
+    E = 0xFF
+};
+
 TEST_CASE("Checking enum class conversion", "[core]")
 {
     using atlas::core::enumToUnderlyingType;
-    Test a{Test::A};
-    Test b{Test::B};
-    Test c{Test::C};
+    TypeTest a{TypeTest::A};
+    TypeTest b{TypeTest::B};
+    TypeTest c{TypeTest::C};
 
     auto var = enumToUnderlyingType(a);
     REQUIRE(var == 0);
@@ -24,4 +33,47 @@ TEST_CASE("Checking enum class conversion", "[core]")
 
     var = enumToUnderlyingType(c);
     REQUIRE(var == 2);
+}
+
+TEST_CASE("Checking enum operator ~", "[core]")
+{
+    using namespace atlas::core;
+
+    OpTest a{OpTest::A};
+    OpTest var;
+    var = ~a;
+    REQUIRE(var == OpTest::E);
+}
+
+TEST_CASE("Checking enum operator |", "[core]")
+{
+    using namespace atlas::core;
+
+    OpTest a{OpTest::B};
+    OpTest b{OpTest::C};
+    OpTest var;
+    var = a | b;
+    REQUIRE(var == OpTest::D);
+}
+
+TEST_CASE("Checking enum operator &", "[core]")
+{
+    using namespace atlas::core;
+
+    OpTest a{OpTest::B};
+    OpTest b{OpTest::C};
+    OpTest var;
+    var = a & b;
+    REQUIRE(var == OpTest::A);
+}
+
+TEST_CASE("Checking enum operator ^", "[core]")
+{
+    using namespace atlas::core;
+
+    OpTest a{OpTest::B};
+    OpTest b{OpTest::C};
+    OpTest var;
+    var = a ^ b;
+    REQUIRE(var == OpTest::D);
 }
