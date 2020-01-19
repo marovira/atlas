50d3d56348285db395851bbd9e1b7876b3289bc3
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2020-01-18 18:40:30 -0800

    [brief] Fixes and tests the Solvers header.
    [detailed]
    - Fixes several typos and errors in the functions and formulas.
    - Removes the static_casts and replaces them with uniform
      initializations because the code wasn't readable.
    - Fixes the quartic function to use a weaker version of the equals
      function used in Atlas. This has the side effect of reporting roots
      several times if their multiplicity is greater than 1.
    - Adds unit tests for solvers.

--- include/atlas/math/Solvers.hpp
+++ include/atlas/math/Solvers.hpp
@@ -1,22 +1,38 @@
 #pragma once
 
-#include "Math.hpp"
-
 #include <atlas/core/Float.hpp>
 
+#include <cassert>
+#include <cmath>
 #include <vector>
 
 namespace atlas::math
 {
+    /**
+     * Solves a quadratic equation of the form Ax^2 + Bx + C.
+     * Note that the coefficients need to be passed in reverse, so {C, B, A}.
+     *
+     * @param coeffs The coefficients of the quadratic equation in reverse.
+     * @param roots The roots of the equation.
+     * @return The number of real roots (if any).
+     */
     template<typename T,
              typename = std::enable_if<std::is_floating_point<T>::value>>
-    int solveQuadric(std::vector<T>& coeffs, std::vector<T>& roots)
+    std::size_t solveQuadratic(std::vector<T> const& coeffs,
+                               std::vector<T>& roots)
     {
         using atlas::core::isZero;
 
+        assert(coeffs.size() >= 3);
+
+        if (roots.empty())
+        {
+            roots.resize(2);
+        }
+
         // Quadratic: x^2 + px + q = 0
         T p, q, D;
-        p = coeffs[1] / (2 * coeffs[2]);
+        p = coeffs[1] / (T{2} * coeffs[2]);
         q = coeffs[0] / coeffs[2];
 
         D = p * p - q;
@@ -26,56 +42,64 @@ namespace atlas::math
             roots[0] = -p;
             return 1;
         }
-        else if (D > static_cast<T>(0.0))
+        else if (D > T{0})
         {
-            T sqrtD  = glm::sqrt(D);
+            T sqrtD  = std::sqrt(D);
             roots[0] = sqrtD - p;
             roots[1] = -sqrtD - p;
             return 2;
         }
-        else
-        {
-            return 0;
-        }
+
+        return 0;
     }
 
+    /**
+     * Solves a cubic equation of the form Ax^3 + Bx^2 + Cx + D. Note that the
+     * coefficients need to be passed in reverse, so {D, C, B, A}.
+     *
+     * @param coeffs The coefficients of the quadratic equation in reverse.
+     * @param roots The roots of the equation.
+     * @return The number of real roots (if any).
+     */
     template<typename T,
              typename = std::enable_if<std::is_floating_point<T>::value>>
-    int solveCubic(std::vector<T>& coeffs, std::vector<T>& roots)
+    std::size_t solveCubic(std::vector<T> const& coeffs, std::vector<T>& roots)
     {
         using atlas::core::isZero;
 
-        int num;
-        T sub;
-        T A, B, C;
-        T sqA, p, q;
-        T cbP, D;
+        assert(coeffs.size() >= 4);
+
+        if (roots.empty())
+        {
+            roots.resize(3);
+        }
+
+        const T pi = 4 * std::atan(T{1});
 
         // Cubic: x^3 + Ax^2 + Bx + C = 0
-        A = coeffs[2] / coeffs[3];
-        B = coeffs[1] / coeffs[3];
-        C = coeffs[0] / coeffs[3];
+        T A = coeffs[2] / coeffs[3];
+        T B = coeffs[1] / coeffs[3];
+        T C = coeffs[0] / coeffs[3];
 
         // Substitute x = y - A/3 to eliminate quadric term:
         // x^3 + px + q = 0
-        sqA = A * A;
-        p   = static_cast<T>(1.0) / static_cast<T>(3.0) *
-            (-static_cast<T>(1.0) / static_cast<T>(3.0) * sqA + B);
-        q = static_cast<T>(1.0) / static_cast<T>(3.0) *
-            (static_cast<T>(2.0) / static_cast<T>(27) * A * sqA -
-             static_cast<T>(1.0) / static_cast<T>(3.0) * A * B + C);
+        T sqA = A * A;
+        T p   = T{1} / T{3} * (-T{1} / T{3} * sqA + B);
+        T q = T{1} / T{2} * (T{2} / T{27} * A * sqA - T{1} / T{3} * A * B + C);
 
         // Use Cardano's formula.
-        cbP = p * p * p;
-        D   = q * q + cbP;
+        T cbP = p * p * p;
+        T D   = q * q + cbP;
 
+        std::size_t num{0};
         if (isZero<T>(D))
         {
             if (isZero<T>(q))
             {
                 // Multiplicity 3.
                 roots[0] = 0;
-                num      = 1;
+                roots.emplace_back(0);
+                num = 1;
             }
             else
             {
@@ -89,19 +113,19 @@ namespace atlas::math
         else if (D < 0)
         {
             // Multiplicity 1 all.
-            T phi = static_cast<T>(1.0) / static_cast<T>(3.0) *
-                    glm::acos(-q / glm::sqrt(-cbP));
-            T t = 2 * glm::sqrt(-p);
+            T phi = T{1} / T{3} * std::acos(-q / std::sqrt(-cbP));
+            T t   = T{2} * std::sqrt(-p);
 
-            roots[0] = t * glm::cos(phi);
-            roots[1] = -t * glm::cos(phi + glm::pi<T>() / static_cast<T>(3));
-            roots[2] = -t * glm::cos(phi - glm::pi<T>() / static_cast<T>(3));
-            num      = 3;
+            const auto piBy3 = pi / static_cast<T>(3);
+            roots[0]         = t * std::cos(phi);
+            roots[1]         = -t * std::cos(phi + piBy3);
+            roots[2]         = -t * std::cos(phi - piBy3);
+            num              = 3;
         }
         else
         {
             // One real solution.
-            T sqrtD = glm::sqrt(D);
+            T sqrtD = std::sqrt(D);
             T u     = std::cbrt(sqrtD - q);
             T v     = -std::cbrt(sqrtD + q);
 
@@ -110,9 +134,9 @@ namespace atlas::math
         }
 
         // Resubstitute.
-        sub = static_cast<T>(1.0) / static_cast<T>(3) * A;
+        T sub = T{1} / T{3} * A;
 
-        for (int i{0}; i < num; ++i)
+        for (std::size_t i{0}; i < num; ++i)
         {
             roots[i] -= sub;
         }
@@ -120,40 +144,59 @@ namespace atlas::math
         return num;
     }
 
+    /**
+     * Solves a quartic equation of the form Ax^4 + Bx^3 + Cx^2 + Dx + E. Note
+     * that the coefficients need to be passed in reverse, so {E, D, C, B, A}.
+     *
+     * Due to the comparison function used, roots of multiplicity greater than
+     * 1 may be returned more than once.
+     *
+     * @param coeffs The coefficients of the quadratic equation in reverse.
+     * @param roots The roots of the equation.
+     * @return The number of real roots (if any).
+     */
     template<typename T,
              typename = std::enable_if<std::is_floating_point<T>::value>>
-    int solveQuartic(std::vector<T>& coeffs, std::vector<T>& roots)
+    std::size_t solveQuartic(std::vector<T> const& coeffs,
+                             std::vector<T>& roots)
     {
         using atlas::core::isZero;
 
+        assert(coeffs.size() == 5);
+
+        auto weakIsZero = [](T x) {
+            return x > -std::numeric_limits<T>::epsilon() &&
+                   x < std::numeric_limits<T>::epsilon();
+        };
+
+        if (roots.empty())
+        {
+            roots.resize(4);
+        }
+
         std::vector<T> c(4);
-        T z, u, v, sub;
-        T A, B, C, D;
-        T sqA, p, q, r;
-        int num;
 
         // Quartic: x^4 + Ax^3 + Bx^2 + Cx + D = 0.
-        A = coeffs[3] / coeffs[4];
-        B = coeffs[2] / coeffs[4];
-        C = coeffs[1] / coeffs[4];
-        D = coeffs[0] / coeffs[4];
+        T A = coeffs[3] / coeffs[4];
+        T B = coeffs[2] / coeffs[4];
+        T C = coeffs[1] / coeffs[4];
+        T D = coeffs[0] / coeffs[4];
 
         // Substitute x = y - A / 4 to eliminate cubic term:
         // x^4 + px^2 + qx + r = 0.
 
-        sqA = A * A;
-        p   = static_cast<T>(3.0) / static_cast<T>(8) * sqA + B;
-        q   = static_cast<T>(1.0) / static_cast<T>(8) * sqA * A -
-            static_cast<T>(1.0) / static_cast<T>(2) * A * B + C;
-        r = -static_cast<T>(3.0) / static_cast<T>(256) * sqA * sqA +
-            static_cast<T>(1.0) / static_cast<T>(16) * sqA * B -
-            static_cast<T>(1.0) / static_cast<T>(4) * A * C + D;
+        T sqA = A * A;
+        T p   = -T{3} / T{8} * sqA + B;
+        T q   = T{1} / T{8} * sqA * A - T{1} / T{2} * A * B + C;
+        T r   = -T{3} / T{256} * sqA * sqA + T{1} / T{16} * sqA * B -
+              T{1} / T{4} * A * C + D;
 
-        if (isZero<T>(r))
+        std::size_t num{0};
+        if (isZero<T>(r) || weakIsZero(r))
         {
             // No absolute term: y(y^3 + py + q) = 0.
-            c[0] = p;
-            c[1] = 1;
+            c[0] = q;
+            c[1] = p;
             c[2] = 0;
             c[3] = 1;
 
@@ -163,41 +206,40 @@ namespace atlas::math
         else
         {
             // Solve the resolvent cubic...
-            c[0] = static_cast<T>(1.0) / static_cast<T>(2) * r * p -
-                   static_cast<T>(1.0) / static_cast<T>(8) * q * q;
+            c[0] = T{1} / T{2} * r * p - T{1} / T{8} * q * q;
             c[1] = -r;
-            c[2] = -static_cast<T>(1.0) / static_cast<T>(2) * p;
+            c[2] = -T{1} / T{2} * p;
             c[3] = 1;
 
             solveCubic<T>(c, roots);
 
             // And take the one real solution
-            z = roots[0];
+            T z = roots[0];
 
             // To build two quadratics.
-            u = z * z - r;
-            v = 2 * z - p;
+            T u = z * z - r;
+            T v = 2 * z - p;
 
-            if (isZero<T>(u))
+            if (isZero<T>(u) || weakIsZero(u))
             {
                 u = 0;
             }
             else if (u > 0)
             {
-                u = glm::sqrt(u);
+                u = std::sqrt(u);
             }
             else
             {
                 return 0;
             }
 
-            if (isZero<T>(v))
+            if (isZero<T>(v) || weakIsZero(v))
             {
                 v = 0;
             }
             else if (v > 0)
             {
-                v = glm::sqrt(v);
+                v = std::sqrt(v);
             }
             else
             {
@@ -208,28 +250,27 @@ namespace atlas::math
             c[1] = (q < 0) ? -v : v;
             c[2] = 1;
 
-            num = solveQuadric<T>(c, roots);
+            num = solveQuadratic<T>(c, roots);
 
             c[0] = z + u;
             c[1] = (q < 0) ? v : -v;
             c[2] = 1;
 
-            std::vector<T> s(2);
-            int tmp = num;
-            num += solveQuadric<T>(c, s);
+            std::vector<T> s(2, T{0});
+            std::size_t tmp = num;
+            num += solveQuadratic<T>(c, s);
             roots[tmp]     = s[0];
             roots[tmp + 1] = s[1];
         }
 
         // Resubstitute.
-        sub = static_cast<T>(1.0) / static_cast<T>(4) * A;
+        T sub = T{1} / T{4} * A;
 
-        for (int i{0}; i < num; ++i)
+        for (std::size_t i{0}; i < num; ++i)
         {
             roots[i] -= sub;
         }
 
         return num;
     }
-
 } // namespace atlas::math
--- test/atlas_math_solvers.cpp
+++ test/atlas_math_solvers.cpp
@@ -1,5 +1,168 @@
+#include <atlas/core/Float.hpp>
 #include <atlas/math/Solvers.hpp>
 
 #include <catch2/catch.hpp>
 
 using namespace atlas::math;
+using atlas::core::areEqual;
+
+TEST_CASE("Testing quadratic solver", "[math]")
+{
+    // x^2 - 1 = 0.
+    //  Two roots: 1, -1.
+    {
+        std::vector<double> coefficients{1.0, 0.0, -1.0};
+        std::vector<double> roots;
+        auto numRoots = solveQuadratic(coefficients, roots);
+
+        REQUIRE(numRoots == 2);
+        REQUIRE(areEqual(roots[0], 1.0));
+        REQUIRE(areEqual(roots[1], -1.0));
+    }
+
+    // x^2 - 2x + 1.
+    // One root: 1.
+    {
+        std::vector<double> coefficients{1.0, -2.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveQuadratic(coefficients, roots);
+
+        REQUIRE(numRoots == 1);
+        REQUIRE(areEqual(roots[0], 1.0));
+    }
+
+    // x^2 + 1.
+    // No real roots.
+    {
+        std::vector<double> coefficients{1.0, 0.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveQuadratic(coefficients, roots);
+
+        REQUIRE(numRoots == 0);
+    }
+}
+
+TEST_CASE("Testing cubic solver", "[math]")
+{
+    // -6 + 11x -6x^2 + x^3.
+    // 3 roots: 1, 2, 3.
+    {
+        std::vector<double> coefficients{-6.0, 11.0, -6.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveCubic(coefficients, roots);
+
+        REQUIRE(numRoots == 3);
+        REQUIRE(areEqual(roots[0], 3.0));
+        REQUIRE(areEqual(roots[1], 2.0));
+        REQUIRE(areEqual(roots[2], 1.0));
+    }
+
+    // -x^2 + x^3.
+    // 2 roots.
+    {
+        std::vector<double> coefficients{0.0, 0.0, -1.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveCubic(coefficients, roots);
+
+        REQUIRE(numRoots == 2);
+        REQUIRE(areEqual(roots[0], 1.0));
+        REQUIRE(areEqual(roots[1], 0.0));
+    }
+
+    // -1 + x^3
+    // 1 real root, two complex.
+    {
+        std::vector<double> coefficients{-1.0, 0.0, 0.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveCubic(coefficients, roots);
+
+        REQUIRE(numRoots == 1);
+        REQUIRE(areEqual(roots[0], 1.0));
+    }
+
+    // -1 + 3x - 3x^2 + x^3.
+    // 1 root.
+    {
+        std::vector<double> coefficients{-1.0, 3.0, -3.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveCubic(coefficients, roots);
+
+        REQUIRE(numRoots == 1);
+        REQUIRE(areEqual(roots[0], 1.0));
+    }
+}
+
+TEST_CASE("Testing quartic solver", "[math]")
+{
+    // 24 + -50x + 35x^2 - 10x^3 + x^4
+    // 4 roots: 1, 2, 3, 4.
+    {
+        std::vector<double> coefficients{24.0, -50.0, 35.0, -10.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveQuartic(coefficients, roots);
+
+        REQUIRE(numRoots == 4);
+        REQUIRE(areEqual(roots[0], 2.0));
+        REQUIRE(areEqual(roots[1], 1.0));
+        REQUIRE(areEqual(roots[2], 4.0));
+        REQUIRE(areEqual(roots[3], 3.0));
+    }
+
+    // 2x^2 - 3x^3 + x^4.
+    // 3 roots: 0, 1, 2.
+    {
+        std::vector<double> coefficients{0.0, 0.0, 2.0, -3.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveQuartic(coefficients, roots);
+
+        REQUIRE(numRoots == 3);
+        REQUIRE(areEqual(roots[0], 2.0));
+        REQUIRE(areEqual(roots[1], 1.0));
+        REQUIRE(areEqual(roots[2], 0.0));
+    }
+
+    // 2 real roots, 2 complex.
+    // 2 -3x + 3x^2 - 3x^3 + x^4
+    {
+        std::vector<double> coefficients{2.0, -3.0, 3.0, -3.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveQuartic(coefficients, roots);
+
+        REQUIRE(numRoots == 2);
+        REQUIRE(areEqual(roots[0], 2.0));
+        REQUIRE(areEqual(roots[1], 1.0));
+    }
+
+    // 1 - 2x^2 + x^4.
+    // 2 real roots.
+    {
+        std::vector<double> coefficients{1.0, 0.0, -2.0, 0.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveQuartic(coefficients, roots);
+
+        REQUIRE(numRoots == 2);
+        REQUIRE(areEqual(roots[0], -1.0));
+        REQUIRE(areEqual(roots[1], 1.0));
+    }
+
+    // 1 - 4x + 6x^2 - 4x^3 + x^4.
+    // 1 root.
+    {
+        std::vector<double> coefficients{1.0, -4.0, 6.0, -4.0, 1.0};
+        std::vector<double> roots;
+        auto numRoots = solveQuartic(coefficients, roots);
+
+        // Due to epsilon values, roots may be reported more than once.
+        REQUIRE((numRoots == 1 || numRoots == 2));
+
+        if (numRoots == 1)
+        {
+            REQUIRE(areEqual(roots[0], 1.0));
+        }
+        else
+        {
+            REQUIRE(areEqual(roots[0], 1.0));
+            REQUIRE(areEqual(roots[1], 1.0));
+        }
+    }
+}
